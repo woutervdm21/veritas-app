63188062a3bf2f420fb9be008b2c8e90448117be
Van Der Merwe <Wouter.VanDerMerwe@SamancorCr.com>
2025-04-17 13:49:01 +0200

    Created Sermon model and Django admin setup

--- church/models.py
+++ church/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Sermon(models.Model):
+    title = models.CharField(max_length=200)
+    pastor = models.CharField(max_length=100)
+    date = models.DateField()
+    audio_link = models.URLField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.speaker}"
